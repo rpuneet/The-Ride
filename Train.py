0672f6c11e76faa2585eadc19313eb9403dad9dc
Puneet Rai <puneetrai04@gmail.com>
2018-06-12 01:10:46 +0530

    Add CSV file support

--- Train.py
+++ Train.py
@@ -1,18 +1,23 @@
-''' This is used to get information for a particular train. '''
+''' This module is used to get information for a particular train. '''
 
-import bs4
+import bs4             # Beautiful Soup for web scraping.
 
-import requests
+import requests        # To get the html of a website.
 
-import pandas as pd
+import pandas as pd    # Storing data in CSV file
 
-from os import getcwd
-
-import json
+import json            # Storing data in JSON file
 
 class Train:
+    ''' A class which stores and retrieves informations about a Train'''
     
     def __init__(self , train_number = "" , start = "today"):
+        ''' Constructor function 
+            Parameters :
+                train_number - Train number of the train.
+                start        - Start day of the train i.e. day when it starts from source station
+        '''
+        
         self.train_details = {"Train Name" : "-" , "Train Number" : "-" , "Source": "-" , "Destination":"-" , "Start Day" : "-"}
         self.train_details["Train Number"] = train_number
         self.train_details["Start Day"] = start
@@ -20,16 +25,23 @@ class Train:
         
         
     def retrieve_details(self):
+        ''' Retrieves details of the train and its route from https://runningstatus.in/ '''
+        
+        
         if self.train_details["Train Number"] == "":
             print("Train number is not assigned.")
             return 0
+        
         print("Retrieving details from runningstatus.in ...." , end = " ")
         
+        # Retrieving data from the website and creating a bs4 object.
         train_details_runningstatus_webpage = requests.get("https://runningstatus.in/status/{}-{}".format(self.train_details["Train Number"] , self.train_details["Start Day"]))
         train_details_bs4 = bs4.BeautifulSoup(train_details_runningstatus_webpage.text , 'lxml')
 
         self.train_details["Train Name"] = train_details_bs4.title.text.split('/')[0]
         
+        
+        ''' Checking if the train number provided is valid or not '''
         if self.train_details["Train Name"] == '':
             print("Invalid Train Number.")
             return 0
@@ -38,6 +50,8 @@ class Train:
         station_fields = ["Station Name" , "Platform" , "Scheduled Arrival" , 
                          "Scheduled Departure" , "Actual Arrival / Departure" ,
                          "Average Speed" , "Train Status"]
+        
+        ''' Storing the table which contains the station details and then adding all staion data to self.stations. '''
         
         table = train_details_bs4.find("table" , {"class" : "table table-striped table-bordered"})
         
@@ -63,8 +77,25 @@ class Train:
         
         print("Done.")
         
+    def store_to_csv(self):
+        '''  Stores the details of the train in a csv file in Train-Details-CSV directory. '''
+        data_frame = pd.DataFrame(self.stations)
+
+        # Handling Fields that are not present. 
+        station_fields = ["Station Name" , "Platform" , "Scheduled Arrival" , 
+                         "Scheduled Departure" , "Actual Arrival / Departure" ,
+                         "Average Speed" , "Train Status"]
+        for field in station_fields:
+            if field not in data_frame:
+                data_frame[field] = ['' for x in self.stations]
+        
+        
+        data_frame = data_frame[station_fields]  #To place the fields in correct order
+        ''' Storing in the csv file '''
+        data_frame.to_csv(".\\Train-Details-CSV\\{}.csv".format(self.train_details["Train Number"]) , index = False)
         
     def store_to_json(self):
+        ''' Stores the details of the train in a json file in Train-Details-Json directory. '''
         with open(".\\Train-Details-Json\\{}.json".format(self.train_details["Train Number"]) , "w") as output_file:
             json.dump(self.__dict__ , output_file)
             
--- Train_Schedule.py
+++ Train_Schedule.py
@@ -1,23 +1,34 @@
 ''' 
-This is the driver program it takes the train number to create a json file for the train details.
+This is the driver program it takes the train number and start day (optional) to create a json file for the train details.
 Train number can be provided as a command line argument or as input in the program
 '''
 
 
-import sys
+import sys              # To get the input from command line
 
-import Train
+import Train            # Module which retrivees and stores data of a particluar train
 
+from os import makedirs # To make the Train-Details-Json directory (This stores all the json files)
+
+
+''' Create a new directory if it doesn't exist ''' 
+os.makedirs("Train-Details-Json" , exist_ok = True) 
+os.makedirs("Train-Details-CSV" , exist_ok = True)
+
+
+''' Checks if train number was provided in the command line or not '''
 if len(sys.argv) > 1:
     train_number = sys.argv[1].strip()
 else:
     train_number = input("Enter train number : ").strip()
 
 start_day = "today"
-
+''' Checks if start day was provided in the command line or not '''
 if len(sys.argv) > 2:
     start_day = sys.argv[2].strip()
 
-train = Train.Train(train_number = train_number , start = start_day)
-train.retrieve_details()
-train.store_to_json()
+
+train = Train.Train(train_number = train_number , start = start_day)  # Initialising Train object
+train.retrieve_details()                                              # Retrieve Train details
+train.store_to_json()                                                 # Store the details in {train number}.json file
+train.store_to_csv()                                                  # Store the details in {train number}.json file  
